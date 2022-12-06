b167ec410c5d483ec98c204f619f70b4e7a427bd
Jimmy Forrester-Fellowes <code@jimmyff.uk>
2022-12-06 13:32:58 +0000

    Day 06

--- bin/day_06.dart
+++ bin/day_06.dart
@@ -4,12 +4,10 @@ import 'dart:io';
 
 // Puzzle: https://adventofcode.com/2022/day/6
 void main(List<String> arguments) {
-  // stream
-  var processed = [];
-
   final datastream = File('assets/day_06_datastream.txt').readAsStringSync();
 
   // find start-of-packet marker
+  var processed = [];
   var data = datastream.split('').takeWhile((value) {
     processed = [value, ...processed].take(4).toList();
     return processed.toSet().length != 4;
@@ -17,6 +15,7 @@ void main(List<String> arguments) {
   print('Part 1: Marker appears after ${data.length + 1} character arrives');
 
   // find message
+  processed = [];
   data = datastream.split('').takeWhile((value) {
     processed = [value, ...processed].take(14).toList();
     return processed.toSet().length != 14;
